405ff1e5736445427f61769ccc14c22b129fd0ce
Raymond Burkholder <raymond@burkholder.net>
2026-07-23 02:56:34 +0000

    handle_signals.hpp refactor into handle_signals.cpp

---
+++ handle_signals.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include "handle_signals.hpp"
+
+net::awaitable<void, executor_type>
+handle_signals( task_group& task_group ) {
+
+  auto executor   = co_await net::this_coro::executor;
+  auto signal_set = net::signal_set{ executor, SIGINT, SIGTERM };
+
+  auto sig = co_await signal_set.async_wait();
+
+  if( sig == SIGINT )
+  {
+    std::cout << "Gracefully cancelling child tasks...\n";
+    task_group.emit(net::cancellation_type::total);
+
+    // Wait a limited time for child tasks to gracefully cancell
+    auto [ec] = co_await task_group.async_wait(
+        net::as_tuple(net::cancel_after(std::chrono::seconds{ 10 })));
+
+    if(ec == net::error::operation_aborted) { // Timeout occurred
+      std::cout << "Sending a terminal cancellation signal...\n";
+      task_group.emit( net::cancellation_type::terminal );
+      co_await task_group.async_wait();
+    }
+
+    std::cout << "Child tasks completed.\n";
+  }
+  else // SIGTERM
+  {
+    net::query(
+      executor.get_inner_executor(),
+      net::execution::context).stop();
+  }
+}
+
--- handle_signals.hpp
+++ handle_signals.hpp
@@ -1,12 +1,6 @@
 
 #pragma once
 
-#include <iostream>
-
-#include <boost/asio.hpp>
-
-#include <boost/beast.hpp>
-
 #include "task_group.hpp"
 
 namespace net       = boost::asio;
@@ -14,35 +8,4 @@ namespace net       = boost::asio;
 using executor_type = net::strand<net::io_context::executor_type>;
 
 net::awaitable<void, executor_type>
-handle_signals(task_group& task_group) {
-
-  auto executor   = co_await net::this_coro::executor;
-  auto signal_set = net::signal_set{ executor, SIGINT, SIGTERM };
-
-  auto sig = co_await signal_set.async_wait();
-
-  if(sig == SIGINT)
-  {
-    std::cout << "Gracefully cancelling child tasks...\n";
-    task_group.emit(net::cancellation_type::total);
-
-    // Wait a limited time for child tasks to gracefully cancell
-    auto [ec] = co_await task_group.async_wait(
-        net::as_tuple(net::cancel_after(std::chrono::seconds{ 10 })));
-
-    if(ec == net::error::operation_aborted) { // Timeout occurred
-      std::cout << "Sending a terminal cancellation signal...\n";
-      task_group.emit(net::cancellation_type::terminal);
-      co_await task_group.async_wait();
-    }
-
-    std::cout << "Child tasks completed.\n";
-  }
-  else // SIGTERM
-  {
-    net::query(
-      executor.get_inner_executor(),
-      net::execution::context).stop();
-  }
-}
-
+handle_signals( task_group& task_group );
