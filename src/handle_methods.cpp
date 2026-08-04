e47b2603d29e4934c098cf0241e48a90eac5c088
Raymond Burkholder <raymond@burkholder.net>
2026-08-04 17:52:08 +0000

    src/ - move files to src

--- src/handle_methods.cpp
+++ src/handle_methods.cpp
@@ -37,6 +37,10 @@ void resource_robots_txt( response_t& response ) {
   response.content_length( content.size() );
 }
 
+void method_head( http::response<http::empty_body>& response ) {
+  response.set( http::field::server, c_sVersion);
+}
+
 void method_get( http::response<http::file_body>& response ) {
   response.set( http::field::server, c_sVersion);
 }
@@ -44,8 +48,4 @@ void method_get( http::response<http::file_body>& response ) {
 void method_post( response_t& response ) {
   response.set( http::field::server, c_sVersion);
   response.body() = "your post was accepted\n";
-}
-
-void method_head( http::response<http::empty_body>& response ) {
-  response.set( http::field::server, c_sVersion);
 }
