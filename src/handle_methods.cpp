8af113dc0df2158d9a177898ac341cd9dbbfb5e6
Raymond Burkholder <raymond@burkholder.net>
2026-08-05 21:58:27 +0000

    src/handle_methods.* - add license header

--- src/handle_methods.cpp
+++ src/handle_methods.cpp
@@ -1,4 +1,25 @@
-#include <string>
+/************************************************************************
+ * Copyright(c) 2026, One Unified. All rights reserved.                 *
+ * email: info@oneunified.net                                           *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+/*
+ * File:    handle_methods.cpp
+ * Author:  raymond@burkholder.net
+ * Project: boost.web
+ * Created: July 22, 2026 18:36
+ */
+
+ #include <string>
 
 //#include <boost/beast/http/field.hpp>
 //#include <boost/beast/http/status.hpp>
--- src/handle_methods.hpp
+++ src/handle_methods.hpp
@@ -1,4 +1,25 @@
-#pragma once
+/************************************************************************
+ * Copyright(c) 2026, One Unified. All rights reserved.                 *
+ * email: info@oneunified.net                                           *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+/*
+ * File:    handle_methods.hpp
+ * Author:  raymond@burkholder.net
+ * Project: boost.web
+ * Created: July 22, 2026 18:36
+ */
+
+ #pragma once
 
 #include <functional>
 
